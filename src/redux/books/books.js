ca901a724fe6cf11ae8cef5d35d28be9cae48a3b
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-20 10:37:19 +0100

    Add the code functionality inside the books.js file

---
+++ src/redux/books/books.js
@@ -0,0 +1,30 @@
+const ADD_BOOK = 'ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+
+export const addedBook = () => ({
+    type: ADD_BOOK,
+});
+
+export const removedBook = () => ({
+    type: REMOVE_BOOK,
+});
+
+const books = [];
+
+export default (state = books, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state, action.payload,
+      ];
+
+    case REMOVE_BOOK:
+      return [
+        ...state.slice(0, action.payload.index),
+        ...state.slice(action.payload.index + 1, state.length),
+      ];
+
+      default:
+        return state;
+  }
+};
