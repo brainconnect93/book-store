7370b621afa901fb5116ec2a6f50247f7371861d
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-20 10:59:41 +0100

    import all necessary method and combine reducer

---
+++ src/redux/configureStore.js
@@ -0,0 +1,14 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit/dist/configureStore';
+import AddandRemoveReducer from './books/books';
+import CategoriesReducer from './categories/categories';
+
+
+const reducer = combineReducers({
+    addAndRemoveBook: AddandRemoveReducer,
+    checkStatus: CategoriesReducer,
+});
+
+const store = configureStore({ reducer });
+
+export default store
