77bca8c1ea5564e3fb2e1f35a08bf38e30c15f33
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-21 00:23:34 +0100

    Add useSelector and useDispatch to the book.js file

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
+import { removedBook } from '../redux/books/books';
 
-const Book = ({ title, author }) => (
-  <ul>
-    <div>{title}</div>
-    <div>{author}</div>
-    <button type="button">Remove</button>
+export default function bookLists() {
+  const displayBooks = useSelector((state) => state.books, shallowEqual);
+  const dispatch = useDispatch();
+
+  const deleteBook = (e) => {
+    const targetId = e.target.id;
+    dispatch(removedBook(targetId));
+  };
+
+  return (
+    <ul>
+      {displayBooks.map((bookList) => (
+        <li key={bookList.id}>
+          <div>{bookList.title}</div>
+          <div>{bookList.author}</div>
+
+          <button 
+          type="button"
+          id={bookList.id}
+          onClick={(e) => deleteBook(e)}>
+            Remove</button>
+
+        </li>
+      ))} 
   </ul>
-
-);
-
-Book.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-};
-
-export default Book;
+  );
+}
