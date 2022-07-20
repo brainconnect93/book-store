3356f5c4036eb9cf2d8b05d38c956e462944c35a
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-20 11:02:05 +0100

    import the store const inside the index.js file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
