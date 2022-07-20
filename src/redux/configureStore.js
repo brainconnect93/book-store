4f20f0fb09a9f70e1b4de760b1197064d103348f
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-20 11:14:36 +0100

    install redux to the dependencies

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,5 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import { configureStore } from '@reduxjs/toolkit/dist/configureStore';
+import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import AddandRemoveReducer from './books/books';
 import CategoriesReducer from './categories/categories';
 
