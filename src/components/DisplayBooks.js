44c9e6528be8ae5dc414cb50b9eab0ad0bbf81d0
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-21 00:47:13 +0100

    add functionality to the displayBook.js file

--- src/components/DisplayBooks.js
+++ src/components/DisplayBooks.js
@@ -1,25 +1,52 @@
 import React, { useState } from 'react';
 import Book from './Books';
 import AddBook from './AddBooks';
+import { useDispatch } from 'react-redux';
+import { addedBook } from '../redux/books/books';
+import { v4 as uuidv4 } from 'uuid';
 
 const DisplayBooks = () => {
-  const [books, setBooks] = useState([]);
+  const dispatch = useDispatch();
 
-  const handleSubmit = () => setBooks();
+  const [books, setBooks] = useState({
+    id: '',
+    title: '',
+    author: '',
+  });
 
-  return (
-    <div>
-      {books.map((books) => (
-        <Book
-          key={books.id}
-          title={books.title}
-          author={books.author}
-        />
-      ))}
+  const onChange = (e) => {
+    setBooks({
+      ...books,
+      [e.target.name]: e.target.value,
+      id: uuidv4(),
+    });
+  };
 
-      <AddBook handleSubmit={handleSubmit} />
-    </div>
-  );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (books.title.trim() && books.author.trim()) {
+      dispatch(addedBook(books));
+    }
+
+      // to clear the input field
+      e.target.parentElement.children[0].value = '';
+      e.target.parentElement.children[1].value = '';
+
+      setBooks({
+        title: '',
+        author: '',
+      });
+    };
+
+    return (
+      <div>
+        <bookLists />
+  
+        <AddBook 
+        handleSubmit={handleSubmit} 
+        onChange={onChange}/>
+      </div>
+    );
 };
 
 export default DisplayBooks;
