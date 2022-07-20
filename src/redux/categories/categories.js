e78be63b1862619ca9acf2d77d91e0dbc9b41306
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-20 10:50:46 +0100

    Add the code functionality to the categories.js file

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,18 @@
+const CHECK_STATUS = 'CHECK_STATUS';
+
+export const checkBookStatus = () => ({
+  type: CHECK_STATUS,
+});
+
+const categories = [];
+
+export default (state = categories, action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return [
+        ...state, "Under construction",
+      ];
+    default:
+      return state;
+  }
+};
